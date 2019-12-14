4cca6c4c76ec64681a03cc842918da05b712e785
Brennon York <brennon.york@gmail.com>
2019-12-14 11:37:01 -0800

    working regex ruleset matching

--- lib/ruleset_apply.py
+++ lib/ruleset_apply.py
@@ -6,25 +6,24 @@ import sys
 ruleset_file = sys.argv[1]
 merge_file   = sys.argv[2]
 
-rule_map = {}
+rule_map = []
 
 with open(ruleset_file, 'r') as rules:
     for rule in rules:
         category, regex = map(lambda x: x.strip(), rule.split(','))
-
-        rule_map[category] = rule_map.get(category, [])
-        rule_map[category].append(regex)
-
-print(rule_map)
+        rule_map.append([category, regex])
 
 with open(merge_file, 'r') as transactions:
     for transaction in transactions:
         d, m, c, p = transaction.split(',', 4)
-        for k, v in rule_map.items():
-            print(k, v)
-            matches = list(map(lambda x: re.match(x, c), v))
-            print("matches are: ", matches, any(matches))
-            if any(matches):
-                sys.stdout.write(d+','+m+','+k+','+p)
-            else:
-                sys.stdout.write(d+','+m+','+c+','+p)
+        regex_matches = list(map(lambda x: re.match(x, m),
+                                 map(lambda y: y[1],
+                                     rule_map)))
+        match_indices = [i for i, j in enumerate(regex_matches) if j]
+
+        # right now first match wins
+        # TODO: make it so *longest* match wins
+        if any(match_indices):
+            sys.stdout.write(d+','+m+','+rule_map[match_indices[0]][0]+','+p)
+        else:
+            sys.stdout.write(d+','+m+','+c+','+p)
